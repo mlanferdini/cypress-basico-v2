25d83896153041d7b871b92c13e63e34dc0a1085
Maurício Lanferdini <mauricio.lanferdini@freterapido.com>
2023-08-22 11:52:08 -0300

    Add testes com request

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -209,4 +209,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('have.value', longText)
       })
 
+      it('faz uma requisição HTTP', function () {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response){
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+                //console.log(response)
+            })    
+       })
+
   })
